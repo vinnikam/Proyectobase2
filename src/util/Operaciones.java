4df68695744492cd5d9829497f3b714791ffb707
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2026-02-12 10:46:40 -0500

    dev : operacion multiplicar

--- src/util/Operaciones.java
+++ src/util/Operaciones.java
@@ -7,4 +7,10 @@ public class Operaciones {
         return null;
 
     }
+    public Long multiplicar (String a, String b){
+        if (!a.isEmpty() && !b.isEmpty())
+            return Long.parseLong(a)+Long.parseLong(b);
+        return null;
+
+    }
 }
