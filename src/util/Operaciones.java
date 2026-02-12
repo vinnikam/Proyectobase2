f05183e540cd2517da97c1cf8e9cd42d0736f217
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2026-02-12 10:44:16 -0500

    operacion restar

--- src/util/Operaciones.java
+++ src/util/Operaciones.java
@@ -7,4 +7,10 @@ public class Operaciones {
         return null;
 
     }
+    public Long restar (String a, String b){
+        if (!a.isEmpty() && !b.isEmpty())
+            return Long.parseLong(a)+Long.parseLong(b);
+        return null;
+
+    }
 }
